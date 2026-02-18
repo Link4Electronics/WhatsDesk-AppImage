c7f60b03558f790e5ff6383fe6db68ff44e1f93f
Link4Electronics <link4electronics@gmail.com>
2026-02-18 13:16:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,9 +24,11 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
-
+if [ "$ARCH" = "aarch64" ]; then
+	make-aur-package electron39-bin
+fi
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of WhatsDesk for aarch64..."
+echo "Making nightly build of WhatsDesk..."
 echo "---------------------------------------------------------------"
 REPO="https://gitlab.com/zerkc/whatsdesk.git"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
